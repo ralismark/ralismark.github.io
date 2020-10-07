c6920de1c6b06540fb25b9b847b32a3913cbed89
ralismark <13449732+ralismark@users.noreply.github.com>
2020-10-07 11:14:19 +1100

    Add header links in ruby

---
+++ _plugins/markdown-header.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  class MarkdownHeader < Converters::Markdown
+    def convert(content)
+      super.gsub(/<h(\d) id="(.*?)">(.*)<\/h(\d)>/, '<h\1 id="\2"><a href="#\2">\3</a></h\1>')
+    end
+  end
+end
