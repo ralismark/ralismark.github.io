c5ac9d9d972cc92c82da94ebc11db83d6b834009
ralismark <13449732+ralismark@users.noreply.github.com>
2021-01-26 12:52:48 +1100

    Make diagrams visible on dark theme

--- _plugins/graphviz.rb
+++ _plugins/graphviz.rb
@@ -1,6 +1,21 @@
 require "svg_optimizer"
 
 module Jekyll
+  class UseCurrentColor < SvgOptimizer::Plugins::Base
+    SELECTOR = %w[circle ellipse line path polygon polyline rect].join(",")
+
+    def process
+      xml.css(SELECTOR).each do |node|
+        node["stroke"] = "currentColor" if node["stroke"] == "black"
+        node["fill"] = "currentColor" if node["fill"] == "black"
+      end
+
+      xml.css("text").each do |node|
+        node["fill"] = "currentColor" if !node["fill"] || node["fill"] = "black"
+      end
+    end
+  end
+
   class GraphBlock < Liquid::Block
 
     def render(context)
@@ -8,7 +23,7 @@ module Jekyll
       io = IO.popen("dot -Gbgcolor=transparent -Tsvg", "r+")
       io.puts(text)
       io.close_write()
-      SvgOptimizer.optimize(io.read())
+      SvgOptimizer.optimize(io.read(), SvgOptimizer::DEFAULT_PLUGINS + [UseCurrentColor])
     end
 
   end
