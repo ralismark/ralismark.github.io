0bb5ce7c41e186713460ba697ffca0367ec60813
ralismark <timmy.yao@outlook.com.au>
2021-01-31 00:02:44 +1100

    Various cleanup

--- _plugins/graphviz.rb
+++ _plugins/graphviz.rb
@@ -1,32 +1,32 @@
 require "svg_optimizer"
 
-module Jekyll
-  class UseCurrentColor < SvgOptimizer::Plugins::Base
-    SELECTOR = %w[circle ellipse line path polygon polyline rect].join(",")
+class UseCurrentColor < SvgOptimizer::Plugins::Base
+  SELECTOR = %w[circle ellipse line path polygon polyline rect].join(",")
 
-    def process
-      xml.css(SELECTOR).each do |node|
-        node["stroke"] = "currentColor" if node["stroke"] == "black"
-        node["fill"] = "currentColor" if node["fill"] == "black"
-      end
-
-      xml.css("text").each do |node|
-        node["fill"] = "currentColor" if !node["fill"] || node["fill"] = "black"
-      end
-    end
-  end
-
-  class GraphBlock < Liquid::Block
-
-    def render(context)
-      text = super
-      io = IO.popen("dot -Gbgcolor=transparent -Tsvg", "r+")
-      io.puts(text)
-      io.close_write()
-      SvgOptimizer.optimize(io.read(), SvgOptimizer::DEFAULT_PLUGINS + [UseCurrentColor])
+  def process
+    xml.css(SELECTOR).each do |node|
+      node["stroke"] = "currentColor" if node["stroke"] == "black"
+      node["fill"] = "currentColor" if node["fill"] == "black"
     end
 
+    xml.css("text").each do |node|
+      node["fill"] = "currentColor" if !node["fill"] || node["fill"] = "black"
+    end
   end
 end
 
-Liquid::Template.register_tag("graph", Jekyll::GraphBlock)
+def svg_optimize(content)
+  SvgOptimizer.optimize(content, SvgOptimizer::DEFAULT_PLUGINS + [UseCurrentColor])
+end
+
+class GraphBlock < Liquid::Block
+  def render(context)
+    text = super
+    io = IO.popen("dot -Gbgcolor=transparent -Tsvg", "r+")
+    io.puts(text)
+    io.close_write()
+    svg_optimize(io.read())
+  end
+end
+
+Liquid::Template.register_tag("graph", GraphBlock)
