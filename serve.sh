34fd5beaa723b3031a2a5831da5dd345593bc25c
ralismark <timmy.yao@outlook.com.au>
2021-03-14 12:32:38 +1100

    Dev/prod split

--- serve.sh
+++ serve.sh
@@ -3,4 +3,4 @@
 IMAGE_NAME=site
 
 docker build -t "$IMAGE_NAME" . &&
-docker run --rm -it -p 4000:4000 --name "$IMAGE_NAME" -v "$PWD:/srv/jekyll:ro" "$IMAGE_NAME" "$@"
+docker run --rm -it -p 4000:4000 --name "$IMAGE_NAME" -v "$PWD:/srv/jekyll:ro" -e JEKYLL_ENV=development "$IMAGE_NAME" "$@"
