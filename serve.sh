7d16fc96d559fc960f2c51c3d79087fce2afa6f2
ralismark <13449732+ralismark@users.noreply.github.com>
2021-06-20 17:13:23 +1000

    Support local caching

--- serve.sh
+++ serve.sh
@@ -2,5 +2,14 @@
 
 IMAGE_NAME=site
 
+# For caching
+mkdir -p {/tmp/_,.}jekyll-cache
+touch {/tmp/_,.}jekyll-metadata
+
 docker build -t "$IMAGE_NAME" . &&
-docker run --rm -it -p 4000:4000 --name "$IMAGE_NAME" -v "$PWD:/srv/jekyll:ro" -e JEKYLL_ENV=development "$IMAGE_NAME" "$@"
+docker run --rm -it -p 4000:4000 --name "$IMAGE_NAME" \
+  -v "$PWD:/srv/jekyll:ro" \
+  -v "/tmp/_jekyll-cache:/srv/jekyll/.jekyll-cache:rw" \
+  -v "/tmp/_jekyll-metadata:/srv/jekyll/.jekyll-metadata:rw" \
+  -e JEKYLL_ENV=development \
+  "$IMAGE_NAME" "$@"
