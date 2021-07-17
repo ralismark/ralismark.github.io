a293cc634d9302177fd6c4922aa1a01752d5f63b
ralismark <13449732+ralismark@users.noreply.github.com>
2021-07-17 11:27:02 +1000

    Use custom rouge formatter

---
+++ _plugins/rouge.rb
@@ -0,0 +1,23 @@
+require 'rouge'
+
+module Rouge
+  module Formatters
+    class Custom < Formatter
+      tag 'custom'
+
+      def initialize(opts={})
+        @formatter = opts[:inline_theme] ? HTMLInline.new(opts[:inline_theme])
+                   : HTML.new
+
+        @formatter = HTMLLineTable.new(@formatter, opts) if opts[:line_numbers]
+
+        # @formatter = opts[:inline_theme] ? HTMLInline.new(opts[:inline_theme]) : HTML.new
+        # @formatter = HTMLLineTable.new(@formatter, opts) if opts[:line_numbers]
+      end
+
+      def stream(tokens, &b)
+        @formatter.stream(tokens, &b)
+      end
+    end
+  end
+end
