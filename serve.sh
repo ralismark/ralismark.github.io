9227d5ec7abde46e707df8e4e97a4a0cceb778d0
ralismark <13449732+ralismark@users.noreply.github.com>
2021-09-15 17:09:21 +1000

    Redo serve script

--- serve.sh
+++ serve.sh
@@ -3,13 +3,13 @@
 IMAGE_NAME=site
 
 # For caching
-mkdir -p {/tmp/_,.}jekyll-cache
-touch {/tmp/_,.}jekyll-metadata
+mkdir -p .jekyll-cache
+touch .jekyll-metadata
 
 docker build -t "$IMAGE_NAME" . &&
 docker run --rm -it -p 4000:4000 --name "$IMAGE_NAME" \
   -v "$PWD:/srv/jekyll:ro" \
-  -v "/tmp/_jekyll-cache:/srv/jekyll/.jekyll-cache:rw" \
-  -v "/tmp/_jekyll-metadata:/srv/jekyll/.jekyll-metadata:rw" \
+  -v "$PWD/.jekyll-cache:/srv/jekyll/.jekyll-cache:rw" \
+  -v "$PWD/.jekyll-metadata:/srv/jekyll/.jekyll-metadata:rw" \
   -e JEKYLL_ENV=development \
   "$IMAGE_NAME" "$@"
