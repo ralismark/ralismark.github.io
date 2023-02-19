901852492e7278d608c45ef8769efb1b7b55fcc1
ralismark <13449732+ralismark@users.noreply.github.com>
2023-02-20 01:12:31 +1100

    Improve svg handling

--- _plugins/graphviz.rb
+++ _plugins/graphviz.rb
@@ -1,10 +1,28 @@
 require "svg_optimizer"
 
 class UseCurrentColor < SvgOptimizer::Plugins::Base
-  SELECTOR = %w[circle ellipse line path polygon polyline rect].join(",")
-
   def process
-    xml.css(SELECTOR).each do |node|
+    xml.root.add_child(
+      <<-EOF
+        <style>
+          svg {
+            color: #e7e6e5;
+          }
+          @media (prefers-color-scheme: light) {
+            svg {
+              color: initial;
+            }
+          }
+          @media print {
+            svg {
+              color: initial;
+            }
+          }
+        </style>
+      EOF
+    )
+
+    xml.css("circle,ellipse,line,path,polygon,polyline,rect").each do |node|
       node["stroke"] = "currentColor" if node["stroke"] == "black"
       node["fill"] = "currentColor" if node["fill"] == "black"
     end
@@ -21,11 +39,40 @@ end
 
 class GraphBlock < Liquid::Block
   def render(context)
-    text = super
+    site = context.registers[:site]
+    #
+    # p context
+    #
+    input = super
     io = IO.popen("dot -Gbgcolor=transparent -Tsvg", "r+")
-    io.puts(text)
+    io.puts(input)
     io.close_write()
-    svg_optimize(io.read())
+    svg = svg_optimize(io.read())
+
+    file = ConstFile.new(site, site.dest, "/assets", "graphviz-#{Digest::SHA1.hexdigest(input)}.svg")
+    file.set_content(svg)
+    site.static_files << file
+
+    file.relative_path
+  end
+end
+
+class ConstFile < Jekyll::StaticFile
+  def modified?
+    return false
+  end
+
+  def set_content(content)
+    @content = content
+  end
+
+  def write(dest)
+    dest_path = destination(dest)
+    FileUtils.mkdir_p(File.dirname(dest_path))
+    File.open(destination(dest), "w") do |file|
+      file.write(@content)
+    end
+    true
   end
 end
 
