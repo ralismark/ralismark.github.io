97f21f31fd41790935d08e8a91fc11615b7e66f8
ralismark <13449732+ralismark@users.noreply.github.com>
2024-04-08 00:17:03 +1000

    heron: Make svgs respect user's color-scheme!

--- heron/recipe/graph.py
+++ heron/recipe/graph.py
@@ -1,25 +1,84 @@
 import dataclasses
+import subprocess
 from pathlib import Path, PurePosixPath
-
-import graphviz
 
 from .. import core
 from ..jinja.registry import jinja_recipe_builder
 from . import inout
+
+FG_SUBST = "#fffff2"
 
 
 @dataclasses.dataclass(frozen=True)
 class GraphvizRecipe(inout.InoutRecipeBase):
     engine: str
 
-    def build_impl(self, ctx: core.BuildContext) -> inout.Inout:
-        # TODO don't fill the background
-        # TODO make the stroke color depend on color-scheme -- use CanvasText maybe?
-        graphviz.render(
-            engine=self.engine,
-            filepath=ctx.input(self.path),
-            outfile=ctx.output(self.opath),
+    def svg_args(self) -> tuple[str, ...]:
+        graph_opts: dict[str, str] = {
+            "fontcolor": FG_SUBST,
+            "color": FG_SUBST,
+            "bgcolor": "none",
+        }
+        node_opts: dict[str, str] = {
+            "fontcolor": FG_SUBST,
+            "color": FG_SUBST,
+        }
+        edge_opts: dict[str, str] = {
+            "fontcolor": FG_SUBST,
+            "color": FG_SUBST,
+        }
+        return (
+            *(f"-G{k}={v}" for k, v in graph_opts.items()),
+            *(f"-N{k}={v}" for k, v in node_opts.items()),
+            *(f"-E{k}={v}" for k, v in edge_opts.items()),
         )
+
+    def cleanup_svg(self, svg: str) -> str:
+        svg = svg.replace(FG_SUBST, "currentColor")
+        svg = svg.replace("</svg>", """
+            <style>:root { color-scheme: light dark; }</style>
+            </svg>
+        """)
+        return svg
+
+    def build_impl(self, ctx: core.BuildContext) -> inout.Inout:
+        outfmt = self.opath.suffix.lstrip(".")
+        if outfmt == "":
+            raise ValueError(f"opath {self.opath} not valid")
+
+        try:
+            if outfmt == "svg":
+                svg = subprocess.run(
+                    [
+                        "dot",
+                        f"-K{self.engine}",
+                        f"-T{outfmt}",
+                        *self.svg_args(),
+                        str(ctx.input(self.path)),
+                    ],
+                    check=True,
+                    capture_output=True,
+                    text=True,
+                ).stdout
+                svg = self.cleanup_svg(svg)
+
+                with ctx.output(self.opath).open("w") as f:
+                    f.write(svg)
+            else:
+                subprocess.run(
+                    [
+                        "dot",
+                        f"-K{self.engine}",
+                        f"-T{outfmt}",
+                        f"-o{ctx.output(self.opath)}",
+                        str(ctx.input(self.path)),
+                    ],
+                    check=True,
+                )
+        except subprocess.CalledProcessError as e:
+            print(e.stderr)
+            raise
+
         return self.inout()
 
     @jinja_recipe_builder("graphviz")
