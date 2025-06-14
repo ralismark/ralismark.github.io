7b77eca0ce0346d4b4e5e08e8a6ddbb2c028b78c
ralismark <13449732+ralismark@users.noreply.github.com>
2025-06-14 14:14:53 +1000

    serve.sh can assume kwellig.garden by default

--- serve.sh
+++ serve.sh
@@ -4,7 +4,6 @@ set -eu
 builddir=$(mktemp -d)
 trap 'rm -rf "$builddir"' EXIT
 
-name=$1
-shift
+name=${1:-kwellig.garden}
 
 HERON_ENV=development nix run . -- dev --out="$builddir" "$name/_heron.py" "$@"
