43dad14d254951a20a4905f9121acb2417155a3c
ralismark <13449732+ralismark@users.noreply.github.com>
2025-06-23 20:08:04 +1000

    heron build should cache

--- serve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-builddir=$(mktemp -d)
-trap 'rm -rf "$builddir"' EXIT
-
-name=${1:-kwellig.garden}
-
-HERON_ENV=development nix run . -- dev --out="$builddir" "$name/_heron.py" "$@"
